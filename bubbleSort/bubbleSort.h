d1fbacc08b22816ca0db2ac85b2bbcd0babd2848
wvxbs <gabriel.ferreira7854@gmail.com>
2025-03-26 20:38:43 -0300

    bubble sort added

---
+++ bubbleSort/bubbleSort.h
@@ -0,0 +1,6 @@
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+
+void bubbleSort(int arr[], int n);
+
+#endif
--- index.c
+++ index.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include "bubbleSort/bubbleSort.h"
 
-void main() {
-    printf("Hello World!\n");
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    printArray(arr, n);
+
+    int arr1[] = {64, 34, 25, 12, 22, 11, 90};
+    bubbleSort(arr1, n);
+    printf("Sorted array (Bubble Sort):\n");
+    printArray(arr1, n);
+
+    return 0;
 }
